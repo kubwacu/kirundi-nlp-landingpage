e7b127ac74dd2e100c36d322095a3e019104d910
kalculata <huzaifa@amplitude.ventures>
2025-06-08 18:16:35 +0200

    Add Lee Fleury Ntacorigira to contributors list

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -3,6 +3,10 @@ const contributors = [
     name: "Huzaifa Nimushimirimana",
     link: "https://www.linkedin.com/in/kalculata/",
   },
+  {
+    name: "Lee Fleury Ntacorigira",
+    link: "https://www.linkedin.com/in/lee-fleury-ntacorigira-4a1057361",
+  }
 ];
 
 export default contributors;
