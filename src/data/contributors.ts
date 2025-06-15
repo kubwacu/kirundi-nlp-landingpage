9db674e613f161486622f9802f7d7d867f906a60
kalculata <huzaifa@amplitude.ventures>
2025-06-15 09:27:45 +0200

    Add multiple contributors to the contributors list

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -6,7 +6,31 @@ const contributors = [
   {
     name: "Lee Fleury Ntacorigira",
     link: "https://www.linkedin.com/in/lee-fleury-ntacorigira-4a1057361",
-  }
+  },
+  {
+    name: "Florian Nosimbola GAHUNGU",
+    link: "https://www.linkedin.com/in/florian-nosimbola-gahungu-aba478286/",
+  },
+  {
+    name: "Alain GUSHAKAKWIMANA",
+    link: "https://www.linkedin.com/in/alain-gushakakwimana-b8429132a/",
+  },
+  {
+    name: "Willy Franck NDAGIJIMANA",
+    link: "https://www.linkedin.com/in/ndagijimana-willy-franck-0ba061364",
+  },
+  {
+    name: "Tony Ariel CUBAHIRO",
+    link: "https://www.linkedin.com/in/tony-ariel-cubahiro-361012275",
+  },
+  {
+    name: "Carmel ISHEMEZWE",
+    link: "https://www.linkedin.com/in/carmel-ishemezwe-b7885b331",
+  },
+  {
+    name: "Béni Crébert BARANSEGETA",
+    link: "https://www.linkedin.com/in/beni-crebert-baransegeta-35799822b"
+  },
 ];
 
 export default contributors;
